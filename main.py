cf305765e30f2de9b961329d07f6cee3688509d2
Xima <igorrx24072004@gmail.com>
2023-10-27 13:28:11 -0300

    Adicionando Menu dos Módulos - main

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from GerenciamentoServicos import GerenciamentoServicos
 
-# Lista de serviços disponíveis relacionados a um posto de gasolina
-servicos_disponiveis = ["Abastecimento de gasolina", "Abastecimento de álcool", "Abastecimento de diesel",
-                       "Troca de óleo", "Balanceamento de rodas", "Lavagem de carro", "Reparo de pneus"]
-
 class MenuPrincipal:
     def __init__(self):
         self.gerenciamento_servicos = GerenciamentoServicos()
@@ -20,10 +16,38 @@ class MenuPrincipal:
     def exibir_menu(self):
         while True:
             print("\nEscolha uma opção:")
+            print("1. Gerenciamento de Serviços")
+            print("2. Estoque de Produtos")
+            print("3. Gestão da Mercearia")
+            print("4. Monitoramento Energético")
+            print("5. Relatórios e Análises")
+            print("6. Sair")
+
+            opcao = input("Digite o número correspondente: ")
+
+            if opcao == "1":
+                self.menu_gerenciamento_servicos()
+            elif opcao == "2":
+                self.menu_estoque_produtos()
+            elif opcao == "3":
+                self.menu_gestao_mercearia()
+            elif opcao == "4":
+                self.menu_monitoramento_energetico()
+            elif opcao == "5":
+                self.menu_relatorio_analise()
+            elif opcao == "6":
+                print("A EcoEnergy agradece a preferência. Volte Sempre!")
+                break
+            else:
+                print("Opção inválida. Tente novamente.")
+
+    def menu_gerenciamento_servicos(self):
+        while True:
+            print("\nMenu Gerenciamento de Serviços:")
             print("1. Agendar serviço")
             print("2. Registrar serviço prestado")
             print("3. Histórico de serviços por cliente")
-            print("4. Sair")
+            print("4. Voltar ao menu principal")
 
             opcao = input("Digite o número correspondente: ")
 
@@ -34,7 +58,7 @@ class MenuPrincipal:
             elif opcao == "3":
                 self.exibir_historico_servicos()
             elif opcao == "4":
-                print("A EcoEnergy agradece a preferência. Volte Sempre!")
+                print("Voltando ao menu principal.")
                 break
             else:
                 print("Opção inválida. Tente novamente.")
@@ -77,6 +101,22 @@ class MenuPrincipal:
         for i, servico in enumerate(self.servicos_disponiveis, start=1):
             print(f"{i}. {servico}")
 
+    def menu_estoque_produtos(self):
+        # Adicione a lógica para o módulo Estoque de Produtos
+        pass
+
+    def menu_gestao_mercearia(self):
+        # Adicione a lógica para o módulo Gestão da Mercearia
+        pass
+
+    def menu_monitoramento_energetico(self):
+        # Adicione a lógica para o módulo Monitoramento Energético
+        pass
+
+    def menu_relatorio_analise(self):
+        # Adicione a lógica para o módulo Relatórios e Análises
+        pass
+
 if __name__ == "__main__":
-    menu = MenuPrincipal()
-    menu.exibir_menu()
+    menu_principal = MenuPrincipal()
+    menu_principal.exibir_menu()
