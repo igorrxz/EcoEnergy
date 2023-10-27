f99133d15a3c16afe76f4c874e597b3d7083e4dc
Xima <igorrx24072004@gmail.com>
2023-10-27 13:13:03 -0300

    Separação de lógica de negócios e interface do usuário - main

--- GerenciamentoServicos.py
+++ GerenciamentoServicos.py
@@ -1,21 +1,17 @@
 class GerenciamentoServicos:
     def __init__(self):
-
         self.servicos_agendados = {}
 
     def agendar_servico(self, cliente, servico):
-        
-        
         if cliente not in self.servicos_agendados:
             self.servicos_agendados[cliente] = []
         self.servicos_agendados[cliente].append(servico)
 
     def registrar_servico(self, cliente, servico, detalhes):
-        
-        pass
+        if cliente in self.servicos_agendados:
+            self.servicos_agendados[cliente].append(f"Serviço Prestado: {servico}, Detalhes: {detalhes}")
 
     def historico_servicos(self, cliente):
-        
         if cliente in self.servicos_agendados:
             return self.servicos_agendados[cliente]
         else:
--- main.py
+++ main.py
@@ -4,57 +4,79 @@ from GerenciamentoServicos import GerenciamentoServicos
 servicos_disponiveis = ["Abastecimento de gasolina", "Abastecimento de álcool", "Abastecimento de diesel",
                        "Troca de óleo", "Balanceamento de rodas", "Lavagem de carro", "Reparo de pneus"]
 
-def menu_principal():
-    gerenciamento_servicos = GerenciamentoServicos()
+class MenuPrincipal:
+    def __init__(self):
+        self.gerenciamento_servicos = GerenciamentoServicos()
+        self.servicos_disponiveis = [
+            "Abastecimento de gasolina",
+            "Abastecimento de álcool",
+            "Abastecimento de diesel",
+            "Troca de óleo",
+            "Balanceamento de rodas",
+            "Lavagem de carro",
+            "Reparo de pneus"
+        ]
 
-    while True:
-        print("\nEscolha uma opção:")
-        print("1. Agendar serviço")
-        print("2. Registrar serviço prestado")
-        print("3. Histórico de serviços por cliente")
-        print("4. Sair")
+    def exibir_menu(self):
+        while True:
+            print("\nEscolha uma opção:")
+            print("1. Agendar serviço")
+            print("2. Registrar serviço prestado")
+            print("3. Histórico de serviços por cliente")
+            print("4. Sair")
 
-        opcao = input("Digite o número correspondente: ")
+            opcao = input("Digite o número correspondente: ")
 
-        if opcao == "1":
-            cliente = input("Digite o nome do cliente: ")
-            print("Escolha um tipo de serviço:")
-            for i, servico in enumerate(servicos_disponiveis, start=1):
-                print(f"{i}. {servico}")
-            servico_escolhido = int(input("Digite o número do serviço desejado: "))
-            if 1 <= servico_escolhido <= len(servicos_disponiveis):
-                servico = servicos_disponiveis[servico_escolhido - 1]
-                gerenciamento_servicos.agendar_servico(cliente, servico)
-                print(f"{servico} agendado com sucesso para {cliente}.")
+            if opcao == "1":
+                self.agendar_servico()
+            elif opcao == "2":
+                self.registrar_servico_prestado()
+            elif opcao == "3":
+                self.exibir_historico_servicos()
+            elif opcao == "4":
+                print("A EcoEnergy agradece a preferência. Volte Sempre!")
+                break
             else:
                 print("Opção inválida. Tente novamente.")
-        elif opcao == "2":
-            cliente = input("Digite o nome do cliente: ")
-            print("Escolha um tipo de serviço prestado:")
-            for i, servico in enumerate(servicos_disponiveis, start=1):
-                print(f"{i}. {servico}")
-            servico_escolhido = int(input("Digite o número do serviço prestado: "))
-            if 1 <= servico_escolhido <= len(servicos_disponiveis):
-                servico = servicos_disponiveis[servico_escolhido - 1]
-                detalhes = input("Digite os detalhes do serviço: ")
-                gerenciamento_servicos.registrar_servico(cliente, servico, detalhes)
-                print(f"{servico} registrado com sucesso para {cliente}.")
-            else:
-                print("Opção inválida. Tente novamente.")
-        elif opcao == "3":
-            cliente = input("Digite o nome do cliente: ")
-            historico = gerenciamento_servicos.historico_servicos(cliente)
-            if historico:
-                print(f"Histórico de serviços para {cliente}:")
-                for i, servico in enumerate(historico, start=1):
-                    print(f"{i}. {servico}")
-            else:
-                print(f"Nenhum serviço encontrado para {cliente}")
-        elif opcao == "4":
-            print("A EcoEnergy agradece a prefêrencia, Volte Sempre!")
-            break
+
+    def agendar_servico(self):
+        cliente = input("Digite o nome do cliente: ")
+        self.mostrar_servicos_disponiveis()
+        servico_escolhido = int(input("Digite o número do serviço desejado: "))
+        if 1 <= servico_escolhido <= len(self.servicos_disponiveis):
+            servico = self.servicos_disponiveis[servico_escolhido - 1]
+            self.gerenciamento_servicos.agendar_servico(cliente, servico)
+            print(f"{servico} agendado com sucesso para {cliente}.")
         else:
             print("Opção inválida. Tente novamente.")
 
+    def registrar_servico_prestado(self):
+        cliente = input("Digite o nome do cliente: ")
+        self.mostrar_servicos_disponiveis()
+        servico_escolhido = int(input("Digite o número do serviço prestado: "))
+        if 1 <= servico_escolhido <= len(self.servicos_disponiveis):
+            servico = self.servicos_disponiveis[servico_escolhido - 1]
+            detalhes = input("Digite os detalhes do serviço: ")
+            self.gerenciamento_servicos.registrar_servico(cliente, servico, detalhes)
+            print(f"{servico} registrado com sucesso para {cliente}.")
+        else:
+            print("Opção inválida. Tente novamente.")
+
+    def exibir_historico_servicos(self):
+        cliente = input("Digite o nome do cliente: ")
+        historico = self.gerenciamento_servicos.historico_servicos(cliente)
+        if historico:
+            print(f"Histórico de serviços para {cliente}:")
+            for i, servico in enumerate(historico, start=1):
+                print(f"{i}. {servico}")
+        else:
+            print(f"Nenhum serviço encontrado para {cliente}")
+
+    def mostrar_servicos_disponiveis(self):
+        print("Escolha um tipo de serviço:")
+        for i, servico in enumerate(self.servicos_disponiveis, start=1):
+            print(f"{i}. {servico}")
+
 if __name__ == "__main__":
-    menu_principal()
+    menu = MenuPrincipal()
+    menu.exibir_menu()
